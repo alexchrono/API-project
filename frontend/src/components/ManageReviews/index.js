1289eb61d37dc5589248ac938f4b3499979082fa
Alex Heasley <alex.heasley@gmail.com>
2023-09-02 17:50:33 -0500

    fixed delete reviews when there were no reviews to delete and also fixed delete reviews in delete reviews modal

--- frontend/src/components/ManageReviews/index.js
+++ frontend/src/components/ManageReviews/index.js
@@ -23,21 +23,26 @@ export default function ManageReviews() {
   let allReviews = useSelector((state) => state.reviews)
   let thisUsersReviews = useSelector((state) => state.reviews.user)
   let thisUser = useSelector((state) => state.session)
-  let actionType="DELETEASPOT"
-  let ourUserId=thisUser.user.userId
+  let [noReviews,setNoReviews]=useState(3)
+  let actionType = "DELETEAREVIEW"
+  let ourUserId = thisUser.user.userId
   const [reloadData, setReloadData] = useState(1);
   useEffect(() => {
     const fetchData = async () => {
       // await ThunkLoadSingle(dispatch, spotId);
-       // reviewReq,spotId,reviewId,objReviews,dispatch
-      await ThunkLoadReviewsByUserId(dispatch,ourUserId)
+      // reviewReq,spotId,reviewId,objReviews,dispatch
+      try{
+      await ThunkLoadReviewsByUserId(dispatch, ourUserId);} catch (error) {
+        setNoReviews(4)
+      }
       // console.log('aboveTHunkLoadReviews with spotId value of', spotId)
 
     };
 
 
     fetchData();
-  }, [dispatch,ourUserId,reloadData]);
+  }, [dispatch, ourUserId, reloadData]);
+
   const handleModalOpen = () => {
     setIsModalOpen(true);
   };
@@ -48,26 +53,31 @@ export default function ManageReviews() {
   };
 
 
-  console.log('THISUSERS REVIEWS IS',thisUsersReviews)
-  let keysToReviews=Object.keys(thisUsersReviews)
-  console.log('KEYS TO REVIEWS IS',keysToReviews)
-  let keysToReviews2=[...keysToReviews]
-  let somethingBig=thisUsersReviews
+  console.log('THISUSERS REVIEWS IS', thisUsersReviews)
+  let keysToReviews = Object.keys(thisUsersReviews)
+  console.log('KEYS TO REVIEWS IS', keysToReviews)
+
+  if (noReviews===4) { return (<h1>You haven't posted any reviews yet</h1>) }
+
+else {
+
+  let keysToReviews2 = [...keysToReviews]
+  let somethingBig = thisUsersReviews
   // console.log('KEYS TO REVIEWS LOOKS LIKE ',keysToReviews)
-//   function checkNoReviewAndCheckNotOwner(){
-//   for(let ele of keysToReviews){
-//     if(thisSpotsReviews[ele]['userId']===thisUser.user.id || thisSpot.ownerId===thisUser.user.id)
-//     {return false}
-//     return true
-//   }
-// }
-let somethingbig=thisUsersReviews
-let data1=JSON.stringify(allReviews)
-let data2=JSON.stringify(thisUsersReviews)
-console.log(`ALL OF THIS USERS REVIEWS IS`,thisUsersReviews)
+  //   function checkNoReviewAndCheckNotOwner(){
+  //   for(let ele of keysToReviews){
+  //     if(thisSpotsReviews[ele]['userId']===thisUser.user.id || thisSpot.ownerId===thisUser.user.id)
+  //     {return false}
+  //     return true
+  //   }
+  // }
+  let somethingbig = thisUsersReviews
+  let data1 = JSON.stringify(allReviews)
+  let data2 = JSON.stringify(thisUsersReviews)
+  console.log(`ALL OF THIS USERS REVIEWS IS`, thisUsersReviews)
   return (
     <>
-    {/* <h1>{data1}</h1>
+      {/* <h1>{data1}</h1>
     <h3>{data2}</h3> */}
 
 
@@ -85,40 +95,41 @@ console.log(`ALL OF THIS USERS REVIEWS IS`,thisUsersReviews)
           </div>
           <div className="monthAndDate">
             <h3>{`${thisUsersReviews[ele]["createdAt"].slice(5, 7)} ${thisUsersReviews[ele]["createdAt"].slice(0, 4)} `}</h3></div>
-            <div className="reviewOfUser">
+          <div className="reviewOfUser">
             <p>
               {thisUsersReviews[ele]["review"]}
             </p>
-            </div>
-            {thisUser.user && typeof thisUsersReviews==="object" &&  thisUsersReviews[ele]['User']['id']===thisUser.user.id && (<>
-        {/* !thisSpotsReviews.find((ele) => ele.userId === thisUser.user.id) && thisSpot.ownerId !== thisUser.user.id */}
-        <OpenModalButton
-          buttonText="Update"
-          // onButtonClick={closeMenu}
-                         // reviewReq,spotId,reviewId,objReviews,dispatch
-                        //  spotId,userId,objReviews,setReloadData,actionType
-
-          modalComponent={<SubmitReviewModal spotId={ele} userId={ele} objReviews={thisUsersReviews} setReloadData={setReloadData} reloadData={reloadData} actionType={"EDITAREVIEW"} onClose={handleModalClose}
-          />
-        }
-        onClick={handleModalOpen}
-        />
-
-        <OpenModalButton
-          buttonText="Delete"
-          // onButtonClick={closeMenu}
-
-
-          modalComponent={<DeleteSpotModal  spotsId={ele} ourArray={thisUsersReviews} actionType={actionType} keysToReviews2={keysToReviews2} setReloadData={setReloadData} onClose={handleModalClose}
-          />
-        }
-        onClick={handleModalOpen}
-        />
-     </> )}
           </div>
+          {thisUser.user && typeof thisUsersReviews === "object" && thisUsersReviews[ele]['User']['id'] === thisUser.user.id && (<>
+            {/* !thisSpotsReviews.find((ele) => ele.userId === thisUser.user.id) && thisSpot.ownerId !== thisUser.user.id */}
+            <OpenModalButton
+              buttonText="Update"
+              // onButtonClick={closeMenu}
+              // reviewReq,spotId,reviewId,objReviews,dispatch
+              //  spotId,userId,objReviews,setReloadData,actionType
 
-))}
+              modalComponent={<SubmitReviewModal spotId={ele} userId={ele} objReviews={thisUsersReviews} setReloadData={setReloadData} reloadData={reloadData} actionType={"EDITAREVIEW"} onClose={handleModalClose}
+              />
+              }
+              onClick={handleModalOpen}
+            />
+
+            <OpenModalButton
+              buttonText="Delete"
+              // onButtonClick={closeMenu}
+
+
+              modalComponent={<DeleteSpotModal spotsId={ele} ourArray={thisUsersReviews} actionType={actionType} keysToReviews2={keysToReviews2} setReloadData={setReloadData} onClose={handleModalClose}
+              />
+              }
+              onClick={handleModalOpen}
+            />
+          </>)}
+        </div>
+
+      ))}
     </>
   );
 
+}
 }
