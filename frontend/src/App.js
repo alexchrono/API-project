2b3b7898c7bcd54132760a8692e3cbc6e582cac2
Alex Heasley <alex.heasley@gmail.com>
2023-10-13 16:02:52 -0500

    looking great  before i mess with landing page header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
-import { Switch,Route } from "react-router-dom";
+import { Switch,Route,useLocation } from "react-router-dom";
 
 import * as sessionActions from "./store/session";
 import Navigation from "./components/Navigation";
@@ -10,24 +10,29 @@ import createSpot from "./components/createSpot";
 import SpotsCurrentUser from "./components/spotsCurrentUserManage";
 import UpdateASpot from "./components/updateAspot";
 import ManageReviews from "./components/ManageReviews";
+import LandingPage from "./components/LandingPage";
 
 function App() {
   const dispatch = useDispatch();
   const [isLoaded, setIsLoaded] = useState(false);
+
+  const location = useLocation(); // get the current location
+  const showNavigation = location.pathname !== "/";
   useEffect(() => {
     dispatch(sessionActions.restoreUser()).then(() => setIsLoaded(true));
   }, [dispatch]);
 
   return (
     <>
-      <Navigation isLoaded={isLoaded} />
+      {showNavigation && <Navigation isLoaded={isLoaded} />}
       {isLoaded && <Switch>
         <Route exact path="/spots/updateAspot/:spotId" component={UpdateASpot} />
         <Route exact path="/spots/manageSpots" component={SpotsCurrentUser} />
         <Route exact path='/reviews/current' component={ManageReviews} />
         <Route exact path="/spots/new" component={createSpot} />
         <Route exact path="/spots/:spotId" component={spot} />
-        <Route exact path="/" component={spots} />
+        <Route exact path="/z" component={spots} />
+        <Route exact path="/" component={LandingPage} />
 
         </Switch>}
     </>
@@ -35,5 +40,3 @@ function App() {
 }
 
 export default App;
-
-
---
+++ frontend/src/components/LandingPage/index.js
@@ -0,0 +1,20 @@
+import './LandingPage.css';
+
+
+export default function LandingPage() {
+
+    return (
+        <>
+            <div id='centered'>
+                <img src={process.env.PUBLIC_URL + '/frontendImages/starfishnobg.png'} id='starfish'></img>
+                <h1 id='mainText'>Island Luxury</h1>
+                </div>
+
+
+
+
+        </>
+    )
+
+
+}
