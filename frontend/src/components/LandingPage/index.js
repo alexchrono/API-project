1eefa36332baeee8324467084d3625a5dda09cd1
Alex Heasley <alex.heasley@gmail.com>
2023-10-13 16:25:09 -0500

    before i try to get subtext in there

--- frontend/src/components/LandingPage/index.js
+++ frontend/src/components/LandingPage/index.js
@@ -8,6 +8,8 @@ export default function LandingPage() {
             <div id='centered'>
                 <img src={process.env.PUBLIC_URL + '/frontendImages/starfishnobg.png'} id='starfish'></img>
                 <h1 id='mainText'>Island Luxury</h1>
+
+                <h6 id='subtext'>Your gateway to paradise</h6>
                 </div>
 
 
